24c9d3f840964d709d9fb8f3d57bbd5ed42c0322
Mario Hernandez <mariohernandezk10@gmail.com>
2021-09-15 11:24:44 -0500

    added styled components

--- list_pokemon/src/components/Pokemon.jsx
+++ list_pokemon/src/components/Pokemon.jsx
@@ -1,30 +1,23 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
 
-function Pokemon() {
-  return (
-    <div className="about">
-      <div className="container">
-        <div className="row align-items-center my-5">
-          <div className="col-lg-7">
-            <img
-              className="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
-          <div className="col-lg-5">
-            <h1 className="font-weight-light">Pokemon</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+const Div = styled.div`
+  margin: 40px;
+  border: 5px outset pink;
+  &:hover {
+   background-color: yellow;
+ }
+`;
+
+const Title = styled.p`
+  font-size: 15px;
+  text-align: center;
+`;
+
+const Pokemon = () => (
+  <Div>
+    <Title>Pokemon</Title>
+  </Div>
+);
 
 export default Pokemon;
